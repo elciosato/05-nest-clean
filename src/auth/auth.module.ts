94048ba9b6c017186809afc009e65ed58d9e1f66
Elcio Yoshiaki Sato <elciosato@gmail.com>
2023-10-23 17:54:13 +0200

    add authentication controller

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { JwtModule } from '@nestjs/jwt'
 import { PassportModule } from '@nestjs/passport'
 import { AuthenticateController } from 'src/controllers/authenticate-controller'
 import { EnvSchema } from 'src/env'
+import { PrismaService } from 'src/prisma/prisma.service'
 
 @Module({
   imports: [
@@ -24,5 +25,6 @@ import { EnvSchema } from 'src/env'
     }),
   ],
   controllers: [AuthenticateController],
+  providers: [PrismaService],
 })
 export class AuthModule {}
