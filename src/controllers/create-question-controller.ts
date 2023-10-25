e6d8a58aed61990b0cb307b63e95aef502a133ad
Elcio Yoshiaki Sato <elciosato@gmail.com>
2023-10-25 14:08:34 +0200

    add user decorator to get user payload

--- src/controllers/create-question-controller.ts
+++ src/controllers/create-question-controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common'
-import { AuthGuard } from '@nestjs/passport'
+import { Controller, Post, UseGuards } from '@nestjs/common'
+import { CurrentUser } from 'src/auth/current-user-decorator'
 import { JwtAuthGuard } from 'src/auth/jwt-auth-guard'
+import { UserPayload } from 'src/auth/jwt-strategy'
 import { PrismaService } from 'src/prisma/prisma.service'
 import { z } from 'zod'
 
@@ -20,7 +21,8 @@ export class CreateQuestionController {
   @Post()
   // @HttpCode(201)
   // @UsePipes(new ZodValidationPipe(createQuestionBodySchema))
-  async handle(@Body() body: CreateQuestionBodySchema) {
+  async handle(@CurrentUser() user: UserPayload) {
+    console.log(user)
     return 'ok'
   }
 }
